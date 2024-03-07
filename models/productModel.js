3e9945e64f5033be9b33de336e20046f49056b6e
Santhosh Srinivasan <santhoshsrinivasan29092001@gmail.com>
2024-03-07 16:51:16 +0530

    Backend Completed

---
+++ models/productModel.js
@@ -0,0 +1,91 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: [true, "Please enter product name"],
+        trim: true,
+        maxLength: [200, "Product name cannot exceed 200 characters"]
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    description: {
+        type: String,
+        required: [true, "Please enter product description"]
+    },
+    ratings: {
+        type: String,
+        default: 0
+    },
+    images: [
+        {
+            image: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    category: {
+        type: String,
+        required: [true, "Please enter product category"],
+        enum: {
+            values: [
+                'Aquarium Accessories',
+                'Aquarium Medicines',
+                'Aquascaping Tools',
+                'Fish Foods',
+                'Fish Tanks & Bowls',
+                'Live Fishes',
+                'Live Pets',
+                'Live Aqua Plants',
+                'Pet Foods',
+                'Pet Needs'
+            ],
+            message : "Please select correct category"
+        }
+    },
+    seller: {
+        type: String,
+        required: [true, "Please enter product seller"]
+    },
+    stock: {
+        type: Number,
+        required: [true, "Please enter product stock"],
+        maxLength: [20, 'Product stock cannot exceed 20']
+    },
+    numOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            user:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            },
+            rating: {
+                type: String,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    user: {
+        type : mongoose.Schema.Types.ObjectId
+    }
+    ,
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    }
+})
+
+let schema = mongoose.model('Product', productSchema)
+
+module.exports = schema
