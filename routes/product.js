0a03db65e1aaf1539fcee24aba80ed4d8f768b32
Santhosh Srinivasan <santhoshsrinivasan29092001@gmail.com>
2024-03-12 11:07:51 +0530

    Backend Product Route Updated

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct, createReview, getReviews, deleteReview, getAdminProducts } = require('../controllers/productController');
 const router = express.Router();
+router.use(express.json());
 const {isAuthenticatedUser, authorizeRoles } = require('../middlewares/authenticate');
 const multer = require('multer');
 const path = require('path')
